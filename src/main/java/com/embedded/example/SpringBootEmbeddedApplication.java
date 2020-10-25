97c889bccbf0029de8ccab2662f28ca6a5a099fa
JK <jatinkansagara@gmail.com>
2020-10-25 15:19:07 +0530

    Initial commit

---
+++ src/main/java/com/embedded/example/SpringBootEmbeddedApplication.java
@@ -0,0 +1,12 @@
+package com.embedded.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmbeddedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmbeddedApplication.class, args);
+	}
+}
